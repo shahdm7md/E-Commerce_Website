86bc32d8e6de26b7c97061b7b3919b31bd69c027
radwa <radwa.wokoa56@gmail.com>
2024-05-03 23:49:01 +0300

    cart

--- testtt/Controllers/CartController.cs
+++ testtt/Controllers/CartController.cs
@@ -1,21 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
 using testtt.Data;
 
 namespace testtt.Controllers
 {
     public class CartController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext context;
 
         public CartController(ApplicationDbContext context)
         {
-            _context = context;
+            this.context = context;
         }
-        public async Task<IActionResult> Index()
+        
+        public  IActionResult Index()
         {
-            var carts = await _context.Carts.ToListAsync();
-            return View(carts);
+            dynamic viewModel= new ExpandoObject();
+            var products =  context.Products.ToList();
+            viewModel.Products = products;
+
+			var carts = context.Carts.ToList();
+			viewModel.Carts = carts;
+
+			var cartitems = context.CartItems.Include(c=>c.Product).Include(c=>c.Cart);
+            viewModel.CartItems = cartitems;
+			return View(viewModel);
         }
+       
+
     }
 }
