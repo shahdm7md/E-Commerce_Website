b9e2e94508b291d542535a866283e481b21bbf4f
Hasnaa khaled <hasnaak97@gmail.com>
2024-05-30 09:13:30 +0300

    Order Details

--- testtt/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ testtt/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -65,6 +65,8 @@ namespace  testtt.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        public static string Orders => "Orders";
+        public static string OrderDetails => "OrderDetails";
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         /// <summary>
@@ -113,6 +115,8 @@ namespace  testtt.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        public static string OrdersNavClass(ViewContext viewContext) => PageNavClass(viewContext, Orders);
+        public static string OrderDetailsNavClass(ViewContext viewContext) => PageNavClass(viewContext, OrderDetails);
         public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
